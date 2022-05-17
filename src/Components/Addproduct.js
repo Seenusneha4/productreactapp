d1b2cc791bc30cfea96570df8fc0078b4e2c3ffa
seenusneha4 <seenaannmathew6@gmail.com>
2022-05-17 21:20:34 +0530

    addproductcomp

--- src/Components/Addproduct.js
+++ src/Components/Addproduct.js
@@ -1,12 +1,17 @@
-<div className="container">
+import React from 'react'
+
+const Addproduct = () => {
+  return (
+    <div>
+        <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="row g-3">
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Product name</label>
                     <input type="text" className="form-control"/>
                 </div>
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Price</label>
                     <input type="text" className="form-control"/>
                 </div>
@@ -20,3 +25,8 @@
         </div>
     </div>
 </div>
+    </div>
+  )
+}
+
+export default Addproduct
