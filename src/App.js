7269574f5981dfeb9950170c9264ffc4fd285874
seenusneha4 <seenaannmathew6@gmail.com>
2022-05-17 21:26:22 +0530

    searching

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addproduct from './Components/Addproduct';
+import Searchproduct from './Components/Searchproduct';
 
 function App() {
   return (
     <div>
-      <Addproduct/>
+      <Searchproduct/>
     </div>
   );
 }
